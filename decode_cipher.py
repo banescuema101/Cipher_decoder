82d1963a46fc8556a1bdda82e170fec1db3edf27
banescuema <banescuema@gmail.com>
2024-09-08 23:27:10 +0300

    Finish my program

--- decode_cipher.py
+++ decode_cipher.py
@@ -1,29 +1,13 @@
 import sys
-# -------> Purposes and behavior:
 
-# I write an Python script that will help you break the cipher and decode the original text. The script does the following:
-#   -> read the ciphertext from a file specified as a command line argument
-#   -> use a dictionary to map each encoded character back to it's original value
-#   -> manually populate this dictionary as you progress in your attempt and reveal new characters
-#   -> whenever you run the script, it prints the text to the screen, with a few minor changes:
-#........ any character that exists as a key in the dictionary is replaced with the found correspondent.
-#........ any replaced character is highlighted in bold red.
-
-
-#   The only known information was the fact that " 5 SLEUZGU " represents a calendar date, so I had to find
-# the month with the fourth letter as well as the seventh. From here I realized that it is NOVEMBER.
-#   From here, everything was like a puzzle, gradually I found other connections
-# (from the prepositions: the, and, on etc.)
-
-# Function for display the text -> red if the letters have changed, black otherwise
-# functie pentru prelucrarea textului pentru afisat, red daca literele s-au schimbat, altfel, negru
-
+#Function for display the text -> red if the letters have changed, black otherwise
 def colored_text(text, cipher_map):
   red_color = '\033[1;31m'
   termination = '\033[0m'
   colored_text = ""
   for char in text:
-    # If the char has been replaced, will be highlighted in red (if i found it through the dictionary values)
+    # If the char has been replaced, will be highlighted in red
+    #  (if i found it through the dictionary values)
     if char in cipher_map.values():
       colored_text += f"{red_color}{char}{termination}"
     else:
@@ -35,7 +19,9 @@ def decode_text(text, cipher_map):
   decoded_text = ""
   for char in text:
     if char in cipher_map:
-      decoded_text += cipher_map[char]  # In the new text i will replace the old char(the key) with the afferent values in the dictionary
+      decoded_text += cipher_map[char]  
+      # In the new text i will replace the old char(the key)
+      #  with the afferent values in the dictionary
     else:
       decoded_text += char
   return decoded_text
